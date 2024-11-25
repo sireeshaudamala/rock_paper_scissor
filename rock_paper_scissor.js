acf5cfadbf7d5f16e5a44fe5e8d0422bdb1b8a32
sireeshaudamala <sireeshaudamala22@gmail.com>
2024-11-25 12:03:20 +0530

    Update rock_paper_scissor.js

--- rock_paper_scissor.js
+++ rock_paper_scissor.js
@@ -1,102 +1,67 @@
-function scissor(computer) {
-  if (computer === 1) {
-    console.log("👎🏻you lose...");
-    console.log("scissor✂️ is smashed by rock🪨");
-    return askForPlay();
-  }
+const ROCK = 1;
+const PAPER = 2;
+const SCISSOR = 3;
 
-  console.log("👍🏻you win 🏆🥇...");
-  console.log("scissor✂️ is tored the paper ");
-  return askForPlay();
+function doesUserWantToPlayAgain() {
+  return confirm("do you want to play again");
 }
 
-function paper(computer) {
-  if (computer === 1) {
-    console.log("👍🏻you win 🏆🥇...");
-    console.log("paper📑 is folded rock🪨");
-    return askForPlay();
-  }
-
-  console.log("👎🏻you lose...");
-  console.log("paper is tore by scissor✂️ ");
-  return askForPlay();
+function isValidInput(userChoice) {
+  return (1 <= userChoice && userChoice <= 3);
 }
 
-function rock(computer) {
-  if (computer === 2) {
-    console.log("👎🏻you lose...");
-    console.log("rock🪨 is folded by paper📑");
-    return askForPlay();
-
+function showGameResult(isUserWon, userChoice, computerChoice) {
+  console.log("computerChoice: " + computerChoice)
+  if (userChoice === computerChoice) {
+    return "tied";
   }
 
-  console.log("👍🏻you win 🏆🥇...");
-  console.log("rock🪨 is smashed scissor✂️");
-  return askForPlay();
+  if (isUserWon) {
+    return "congratulations!, you won.";
+  }
+
+  return "oh sorry! , you loose.";
 }
 
-function askForPlay() {
-  console.log("\n\n------------****-------------");
-  if (confirm("do you want to play again")) {
-    console.clear()
-    return playGame();
+function doesUserWon(userChoice, computerChoice) {
+  const choicePairs = userChoice + computerChoice;
+
+  return choicePairs === "13" || choicePairs === "21" || choicePairs === "32";
+}
+
+function getUserChoice() {
+  const userChoice = +prompt("enter number to select 1.rock,2.paper,3.scissor");
+  if (!isValidInput(userChoice)) {
+    console.log("Invalid input! Please enter a number ");
+    return getUserChoice();
   }
 
-  return "thanks for playing";
+  return userChoice;
+}
+
+function getComputerChoice() {
+  return Math.ceil(Math.random() * 3);
+}
+
+function welcomeMessage() {
+  console.clear();
+  
+  return "--welcome to rock🪨 paper📑 scissor✂️ game ----";
 }
 
 function playGame() {
-  console.log("-- rock🪨, paper, scissor✂️, shoot! On shoot --- ");
-  const user = +prompt("enter number: ");
-  const computer = Math.ceil(Math.random() * 3);
+  console.log(welcomeMessage());
 
-  if (user === computer) {
-    console.log("the game is tied let's play again to break the tie");
-    return playGame();
-  }
-  console.log("computer number " + computer);
+  const userChoice = getUserChoice();
+  const computerChoice = getComputerChoice();
+  const isUserWon = doesUserWon(userChoice, computerChoice);
+  console.log(showGameResult(isUserWon, userChoice, computerChoice));
 
-  if (user === 1) {
-    return rock(computer);
+  if (!doesUserWantToPlayAgain()) {
+    return "thanks for playing";
   }
 
-  if (user === 2) {
-    return paper(computer);
-  }
-
-  if (user === 3) {
-    return scissor(computer);
-  }
-
-  console.log("Invalid input! Please enter a number ");
   return playGame();
 }
 
-function askHelp() {
-  const help = prompt("if you don't know my rules type help😣:");
-
-  if (help === "help") {
-    console.log("1 for rock🪨 \n2 for paper📑\n3 for scissor✂️");
-  }
-
-  return playGame()
-}
-
-function greetings() {
-  const name = prompt("enter name :");
-  console.log("hello! " + name);
-
-  if (confirm("do you want to play game")) {
-    console.log("--welcome to rock🪨 paper📑 scissor✂️ game ----");
-    return askHelp();
-  }
-
-  if (confirm("don't you have courage to play?")) {
-    return "you don't have luck to play get out from here '" + name + "'";
-  }
-
-  console.log("then play🤦‍♂️");
-  return askHelp();
-}
-
-console.log(greetings());
+playGame();
